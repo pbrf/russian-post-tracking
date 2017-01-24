96536f4b39f227d1fc27588dec64364c105908b3
Dmitry Kalinin <dmitry.kalinin.email@gmail.com>
2017-01-24 13:21:18 +0200

    1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='russian-post-tracking',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     author='Dmitry Kalinin',
     author_email='vladimirovich@pbrf.ru',
